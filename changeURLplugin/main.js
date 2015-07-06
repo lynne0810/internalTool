2ccf64e423248ffad7b9c20a96d0475fa0025f56
linghao gu <linghao@yahoo-inc.com>
2015-07-06 16:17:28 -0700

    demov1.0

--- changeURLplugin/main.js
+++ changeURLplugin/main.js
@@ -1,62 +1,13 @@
-var json = {
-	'protocal' : "http:",
-	'dev1' : 'disgustmust.corp.gq1.yahoo.com',
-	'dev2' : 'nameblame.corp.gq1.yahoo.com',
-	'host1' : 'us.dev-current.web.search.yahoo.com',
-	'host2' : 'us.dd.web.search.yahoo.com',
-	'path1' : "search",
-	'path2' : "search/ads"  
-};
+document.getElementById("inputPad").onkeypress = handleKeyPress;
 
-/**
- * parse the string to an object
- * the format for the string should be dev1host1dev2
- * the object should be the object for location
- */
-function parseString(location, callback){
-	location = location.replace(" ", "");
-	location = location.toLowerCase();
-	var nameObj = new Array();
-	//every para should end up with a number, the format is [a-z]*[1-9]
-	var locationArray = new Array();
-	var start = 0;
-	var end = 0;
-	for (var i = 0; i < location.length; i++){
-		if(location[i] <= "9" && location[i] >= "0"){
-			var temp = location.substr(start, end - start + 1);
-			nameObj[temp] = json[temp + location[i]];
-			start = end + 2;
-		}else{
-			end = i;
-		}
-	}
-
-	callback(window, nameObj);
-}
-
-function changeURL(myWindow, dataObj){
-	if(dataObj["host"] !== undefined){
-		myWindow.location.host = dataObj["host"];
+function handleKeyPress(e){
+	if(e.keyCode === 13){
+		//alert("Enter is pressed");
+		var text = document.getElementById("inputPad").value;
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  			chrome.tabs.sendMessage(tabs[0].id, {greeting: "modifyURL", content: text}, function(response) {
+    			console.log(response.farewell);
+ 			});
+		});
 	}
 }
-
-function getCurrentTabUrl() {
-  // Query filter to be passed to chrome.tabs.query - see
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-}
-
-function renderStatus(statusText) {
-	document.getElementById('status').innerHTML = statusText;
-  getCurrentTabUrl();
-}
-
-document.addEventListener('DOMContentLoaded', renderStatus("404"));
-
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    console.log(response.farewell);
-  });
-});
--- changeURLplugin/page.js
+++ changeURLplugin/page.js
@@ -1,8 +1,61 @@
+var json = {
+	'protocal' : "http:",
+	'dev1' : 'disgustmust.corp.gq1.yahoo.com',
+	'dev2' : 'nameblame.corp.gq1.yahoo.com',
+	'host1' : 'us.dev-current.web.search.yahoo.com',
+	'host2' : 'us.dd.web.search.yahoo.com',
+	'path1' : "search",
+	'path2' : "search/ads"  
+};
+
+/**
+ * parse the string to an object
+ * the format for the string should be dev1host1dev2
+ * the object should be the object for location
+ */
+function parseString(location, callback){
+	location = location.replace(" ", "");
+	location = location.toLowerCase();
+	var nameObj = new Array();
+	//every para should end up with a number, the format is [a-z]*[1-9]
+	var locationArray = new Array();
+	var start = 0;
+	var end = 0;
+	for (var i = 0; i < location.length; i++){
+		if(location[i] <= "9" && location[i] >= "0"){
+			var temp = location.substr(start, end - start + 1);
+			nameObj[temp] = json[temp + location[i]];
+			start = end + 2;
+		}else{
+			end = i;
+		}
+	}
+
+	callback(window, nameObj);
+}
+
+function changeURL(myWindow, dataObj){
+	console.log(dataObj);
+	if(dataObj["host"] !== undefined){
+		myWindow.location.host = dataObj["host"];
+	}
+	if(dataObj["path"] !== undefined){
+		myWindow.location.pathname = dataObj["path"];
+	}
+}
+
+function modifyURL(text){
+	parseString(text, changeURL);
+}
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  });
+    console.log(request, sender, sendResponse);
+    if (request.greeting == "modifyURL"){
+    	modifyURL(request.content);
+    	sendResponse({farewell: "good"});
+    }
+});
